46103285db75906e2c2c69e694e38bf71d21802e
Aadidev Soni <aadidev@Aadidevs-MacBook-Pro.local>
2025-12-10 11:48:37 +0530

    Remote Procedure Calls

--- Assets/Scripts/TestNetwork.cs
+++ Assets/Scripts/TestNetwork.cs
@@ -15,14 +15,49 @@ public class TestNetwork : NetworkIdentity
     // Start of Color
     private void Update()
     {
+        /*
         if (Input.GetKeyDown(KeyCode.A))
-            SetColor();
+            //SetColor() - for local color
+            SetColor(_color);
+        */
+
+        //Using Structs
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            var myStruct = new TestStruct()
+            {
+                color = _color,
+                intValue = 10
+            };
+            SetColor(myStruct);
+        }
     }
 
-    private void SetColor()
+    //[ServerRpc] // If this is not there, only server will run it and it wont b synchroized with the clients
+    //Now, if we press A in client, it i nstructs the server to run the SetColor and color chanmges in Server only
+
+    [ObserversRpc(bufferLast: true)] // This makes the server as well as all the clients to run the method
+    // private void SetColor() - This makes it such that when client hits A, server and client has their local colors that might not be synced
+    private void SetColor(TestStruct myStruct)
     {
-        _renderer.material.color = _color;
+        _renderer.material.color = myStruct.color;
     }
+
+    //RPCs - command or instruction ffor someone to run some code. Instruct ServerRpc to run this function
+    // ServerRpc, ObserversRpc and TargetRpc
+    // ServerPc: Client or Server -> Server
+    // ObserversRpc: Serveror Client -> All clients 
+    // TargetRpc: Server or Client -> single client
+
+    //Stroing global variables in structs
+    public struct TestStruct
+    {
+        public Color color;
+        public int intValue;
+    }
+
+    // If a new player joins, any instruction that  changed the colro of cube befgore wont be applicable to this player to fix this:
+    // TO do this, we add bufferlast: true in the ObserversRpc
 
     //End of Color
 
