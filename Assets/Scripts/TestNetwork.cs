49dab9bfd34a60470887d189234ee9125354aeda
Aadidev Soni <aadidev@Aadidevs-MacBook-Pro.local>
2025-12-10 09:55:05 +0530

    SetColor unsinchronized

--- Assets/Scripts/TestNetwork.cs
+++ Assets/Scripts/TestNetwork.cs
@@ -5,22 +5,39 @@ using PurrNet;
 
 public class TestNetwork : NetworkIdentity
 {
+    // Spawning a cube
     [SerializeField] private NetworkIdentity _networkIdentity;
 
-    private void Awake()
-    {
+    // Setting the color of the cube
+    [SerializeField] private Color _color;
+    [SerializeField] private Renderer _renderer;
 
+    // Start of Color
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+            SetColor();
     }
 
-    private void Start()
+    private void SetColor()
     {
+        _renderer.material.color = _color;
+    }
 
+    //End of Color
+
+    // Start of Spawn
+    private void Awake()
+    {
+        //This givce false and shows that the server only spawns objects once initial setup is done
+        Debug.Log($"IsSpawned: {isSpawned}");
     }
 
     protected override void OnSpawned()
     {
         base.OnSpawned();
 
+        //If below code is not there, the spaning will be done by both server and all the clients and hence it will spawn it multple times
         if (!isServer)
             return;
 
@@ -28,4 +45,6 @@ public class TestNetwork : NetworkIdentity
         //If an object has NetworkIdentity, it will be spawned
 
     }
+
+    // End of Spawn
 }
